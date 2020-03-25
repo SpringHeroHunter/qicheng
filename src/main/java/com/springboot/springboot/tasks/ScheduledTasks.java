4cdf2770bc5fa3784c2fadf5dda8f7402134f877
mazhao <mazhao@haoma.com>
2020-03-25 18:07:06 +0800

    qicheng

--- src/main/java/com/springboot/springboot/tasks/ScheduledTasks.java
+++ src/main/java/com/springboot/springboot/tasks/ScheduledTasks.java
@@ -18,7 +18,7 @@ public class ScheduledTasks {
      * @Scheduled(initialDelay=1000, fixedRate=5000) ：第一次延迟1秒后执行，之后按fixedRate的规则每5秒执行一次
      * @Scheduled(cron=” /5 “) ：通过cron表达式定义规则
      */
-    @Scheduled(fixedRate = 5000)
+    @Scheduled(fixedRate = 50000000)
     public void reportCurrentTime() {
         log.info("This is now{}",dateFormat.format(new Date()));
     }
