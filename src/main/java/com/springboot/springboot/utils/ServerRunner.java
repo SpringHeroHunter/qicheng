19aa47ceeeffa1c320b6c3aeffbd6b5721e407dc
mazhao <mazhao@haoma.com>
2020-03-31 11:13:20 +0800

    qicheng

--- src/main/java/com/springboot/springboot/utils/ServerRunner.java
+++ src/main/java/com/springboot/springboot/utils/ServerRunner.java
@@ -1,11 +1,17 @@
 package com.springboot.springboot.utils;
 
 import com.corundumstudio.socketio.SocketIOServer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+
+/**
+ * socket启动类
+ */
 @Component
+@Slf4j
 public class ServerRunner implements CommandLineRunner {
     private final SocketIOServer server;
     @Autowired
@@ -16,5 +22,6 @@ public class ServerRunner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         server.start();
+        log.info("socket.io启动成功！");
     }
 }
