de74b39c2d6febb2f4275df4cf555ab88e9e463c
mazhao <mazhao@haoma.com>
2020-07-09 11:29:53 +0800

    excel解析

--- src/main/java/com/springboot/springboot/controller/ExcelController.java
+++ src/main/java/com/springboot/springboot/controller/ExcelController.java
@@ -5,17 +5,24 @@ import com.alibaba.excel.read.builder.ExcelReaderBuilder;
 import com.alibaba.excel.write.builder.ExcelWriterBuilder;
 import com.alibaba.excel.write.builder.ExcelWriterSheetBuilder;
 import com.springboot.springboot.entity.DemoData;
+import com.springboot.springboot.entity.FileData;
 import com.springboot.springboot.excelListener.DemoReadListener;
 import com.springboot.springboot.excelListener.WebDataListener;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -70,6 +77,7 @@ public class ExcelController {
     }
 
 
+
     /**
      * excel模板下载
      * @param response
@@ -92,20 +100,68 @@ public class ExcelController {
     }
 
 
+    /**
+     * excel模板填充
+     */
+    @PostMapping("template")
+    public void template(){
+        // 加载模板
+        InputStream templateFile = this.getClass().getClassLoader().getResourceAsStream("templates/fill_data_template1.xlsx");
+        // 写入文件
+        String targetFileName = "单组数据填充.xlsx";
+        // 准备对象数据填充 对象
+//        FileData data = new FileData();
+//        data.setName("mazhao");
+//        data.setAge(18);
+
+        // 生成工作簿对象
+        ExcelWriterBuilder workBookWriter= EasyExcel.write(targetFileName).withTemplate(templateFile);
+        // 获取工作表并填充 map
+        HashMap<String, String> mapFileData = new HashMap<>();
+        mapFileData.put("name", "mazhao");
+        mapFileData.put("age", "18");
+        // 获取第一个工作表填充并自动关闭流
+        workBookWriter.sheet().doFill(mapFileData);
+    }
+
+
+    /**
+     * excel多组数据模板填充
+     */
+    @PostMapping("numtemplate")
+    public void numtemplate(){
+        // 加载模板
+        InputStream templateFile = this.getClass().getClassLoader().getResourceAsStream("templates/fill_data_template1.xlsx");
+        // 写入文件
+        String targetFileName = "多组数据填充.xlsx";
+        // 生成工作簿对象
+        ExcelWriterBuilder workBookWriter = EasyExcel.write(targetFileName).withTemplate(templateFile);
+        // 获取第一个工作表填充并自动关闭流
+        workBookWriter.sheet().doFill(TemplateData());
+    }
 
 
 
 
+    /**
+     * 多组数据填充准备
+     * @return
+     */
+    public static List<FileData> TemplateData() {
+        ArrayList<FileData> fillDatas = new ArrayList<FileData>();
+        for (int i = 0; i < 10; i++) {
+            FileData fillData = new FileData();
+            fillData.setName("杭州黑马0" + i);
+            fillData.setAge(10 + i);
+            fillDatas.add(fillData);
+        }
+        return fillDatas;
+    }
 
-
-
-
-
-
-
-
-
-
+    /**
+     * 读取数据准备
+     * @return
+     */
     public static List<DemoData> data() {
         List<DemoData> list = new ArrayList<DemoData>();
         for (int i = 0; i < 1000; i++) {
