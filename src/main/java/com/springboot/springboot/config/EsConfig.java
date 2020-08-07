41a10b53c50e09e05ea43883d0d70c462241569d
mazhao <mazhao@haoma.com>
2020-08-07 14:16:22 +0800

    excel解析

---
+++ src/main/java/com/springboot/springboot/config/EsConfig.java
@@ -0,0 +1,30 @@
+package com.springboot.springboot.config;
+
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.config.ElasticsearchConfigurationSupport;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Configuration
+public class EsConfig extends ElasticsearchConfigurationSupport {
+    @Bean
+    public Client elasticsearchClient() throws UnknownHostException {
+        Settings settings = Settings.builder().put("cluster.name", "my-application").build();
+        TransportClient client = new PreBuiltTransportClient(settings);
+        client.addTransportAddress(new TransportAddress(InetAddress.getByName("39.106.183.125"), 9300));
+        return client;
+    }
+
+    @Bean(name = { "elasticsearchOperations", "elasticsearchTemplate" })
+    public ElasticsearchTemplate elasticsearchTemplate() throws UnknownHostException {
+        return new ElasticsearchTemplate(elasticsearchClient());
+    }
+}
